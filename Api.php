3b2121a6401da3b34c6d0997ed7983bc7c5cea12
brunocriacoes <br.rafael@outlook.com>
2021-07-30 17:51:49 -0300

    CURL

--- Api.php
+++ Api.php
@@ -2,23 +2,20 @@
 
 class Api {
     static function _licence( $localizador ) {
-        $path = "https://licenca.infinitisistemas.com.br";
+        $path = "http://licenca.infinitisistemas.com.br";
         $path .= "/localizaregistro.php";
         $path .= "?localizador={$localizador}";
-        $response_string_json = file_get_contents($path);
+        $response_string_json = get_curl($path);
         $response_string_json = utf8_decode($response_string_json);
         $response_string_json = str_replace('?', '', $response_string_json);
         $response = json_decode($response_string_json, true);
         $host = $response[0]['HOST_REMOTO'];
-        $port = $response[0]['PORTA'];
-        set_log( "LICENCA -> {$path}");
-        set_log( " URL -> http://{$host}:{$port}");
+        $port = $response[0]['PORTA'];        
         return "http://{$host}:{$port}";
     }
     static function _welcome() {
         $welcome_base64 = $_REQUEST['dwwelcomemessage'];
         $welcome_json_string = base64_decode($welcome_base64);
-        set_log( " WELCOME -> {$welcome_json_string}" );
         $welcome = json_decode( $welcome_json_string, true );
         return $welcome;
     }
@@ -26,93 +23,91 @@ class Api {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        $res = file_get_contents( "{$host}/login?{$query_string}" );
-        set_log( "POST -> {$host}/login?{$query_string}" );
-        set_log( "RES -> {$res}" );
+        $res = get_curl( "{$host}/login?{$query_string}" );
         echo $res;
     }
     static function lernomes( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lernomes?{$query_string}" );
+        echo get_curl( "{$host}/lernomes?{$query_string}" );
     }
     static function lerpecasnome( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecasnome?{$query_string}" );
+        echo get_curl( "{$host}/lerpecasnome?{$query_string}" );
     }
     static function lerpecasIdentNome( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecasIdentNome?{$query_string}" );
+        echo get_curl( "{$host}/lerpecasIdentNome?{$query_string}" );
     }
     static function entregarpecaident( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/entregarpecaident?{$query_string}" );
+        echo get_curl( "{$host}/entregarpecaident?{$query_string}" );
     }
     static function devolverpecaident( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/devolverpecaident?{$query_string}" );
+        echo get_curl( "{$host}/devolverpecaident?{$query_string}" );
     }
     static function lerpecaident( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecaident?{$query_string}" );
+        echo get_curl( "{$host}/lerpecaident?{$query_string}" );
     }
     static function lerpecas( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecas?{$query_string}" );
+        echo get_curl( "{$host}/lerpecas?{$query_string}" );
     }
     static function lerpecasestoque( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecasestoque?{$query_string}" );
+        echo get_curl( "{$host}/lerpecasestoque?{$query_string}" );
     }
     static function lerpecasuso( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecasuso?{$query_string}" );
+        echo get_curl( "{$host}/lerpecasuso?{$query_string}" );
     }
     static function lerlocais( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerlocais?{$query_string}" );
+        echo get_curl( "{$host}/lerlocais?{$query_string}" );
     }
     static function lerpecaslocal( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lerpecaslocal?{$query_string}" );
+        echo get_curl( "{$host}/lerpecaslocal?{$query_string}" );
     }
     static function movimentarpecalocal( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/movimentarpecalocal?{$query_string}" );
+        echo get_curl( "{$host}/movimentarpecalocal?{$query_string}" );
     }
     static function lermovimentacaopeca( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lermovimentacaopeca?{$query_string}" );
+        echo get_curl( "{$host}/lermovimentacaopeca?{$query_string}" );
     }
     static function lermotivos( $query_string ) {
         $welcome = Api::_welcome();
         $localizador = $welcome['localizador'];
         $host = Api::_licence($localizador);
-        echo file_get_contents( "{$host}/lermotivos?{$query_string}" );
+        echo get_curl( "{$host}/lermotivos?{$query_string}" );
     }
 }
--- index.php
+++ index.php
@@ -3,37 +3,60 @@
 header('Content-Type: text/html; charset=utf-8');
 header('Access-Control-Allow-Origin: *');
 
-function set_log( $mensagem ) {
-    file_put_contents( __DIR__ . "/log.txt", date('d/m/Y H:i') ." {$mensagem} \n",  FILE_APPEND  );
+function set_log($mensagem)
+{
+    file_put_contents(__DIR__ . "/log.txt", date('d/m/Y H:i') . " {$mensagem} \n",  FILE_APPEND);
+}
+
+function get_curl($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    curl_setopt($ch, CURLOPT_HEADER         ,false);
+    curl_setopt($ch, CURLOPT_FRESH_CONNECT  ,true);
+    curl_setopt($ch, CURLOPT_FORBID_REUSE   ,true);
+    curl_setopt($ch, CURLOPT_TIMEOUT        ,30);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER ,false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST ,false);
+
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
 }
 
 include __DIR__ . '/Api.php';
 
-function router( $path, $action_hook ) {
+function router($path, $action_hook)
+{
     $corruente_uri = $_SERVER['REQUEST_URI'];
     $corruente_path = parse_url($corruente_uri, PHP_URL_PATH);
     $corruente_quary = parse_url($corruente_uri, PHP_URL_QUERY);
-    if( $corruente_path == $path ) {
-        $explode_method_instace = explode( '@', $action_hook );
-        call_user_func( $explode_method_instace, urldecode( $corruente_quary ) );
+    
+    if ($corruente_path == $path) {
+        $explode_method_instace = explode('@', $action_hook);
+        call_user_func($explode_method_instace, urldecode($corruente_quary));
         return;
     }
 }
 
 
-router('/login', 'Api@login' );
-router('/lernomes', 'Api@lernomes' );
-router('/lerpecasnome', 'Api@lerpecasnome' );
-router('/lerpecasIdentNome', 'Api@lerpecasIdentNome' );
-router('/entregarpecaident', 'Api@entregarpecaident' );
-router('/devolverpecaident', 'Api@devolverpecaident' );
-router('/lerpecaident', 'Api@lerpecaident' );
-router('/lerpecas', 'Api@lerpecas' );
-router('/lerpecasestoque', 'Api@lerpecasestoque' );
-router('/lerpecasuso', 'Api@lerpecasuso' );
-router('/lerlocais', 'Api@lerlocais' );
-router('/lerpecaslocal', 'Api@lerpecaslocal' );
-router('/movimentarpecalocal', 'Api@movimentarpecalocal' );
-router('/lermovimentacaopeca', 'Api@lermovimentacaopeca' );
-router('lermotivos', 'Api@lermotivos' );
-router('/devolverpecaident', 'Api@devolverpecaident' );
+router('/login', 'Api@login');
+router('/lernomes', 'Api@lernomes');
+router('/lerpecasnome', 'Api@lerpecasnome');
+router('/lerpecasIdentNome', 'Api@lerpecasIdentNome');
+router('/entregarpecaident', 'Api@entregarpecaident');
+router('/devolverpecaident', 'Api@devolverpecaident');
+router('/lerpecaident', 'Api@lerpecaident');
+router('/lerpecas', 'Api@lerpecas');
+router('/lerpecasestoque', 'Api@lerpecasestoque');
+router('/lerpecasuso', 'Api@lerpecasuso');
+router('/lerlocais', 'Api@lerlocais');
+router('/lerpecaslocal', 'Api@lerpecaslocal');
+router('/movimentarpecalocal', 'Api@movimentarpecalocal');
+router('/lermovimentacaopeca', 'Api@lermovimentacaopeca');
+router('lermotivos', 'Api@lermotivos');
+router('/devolverpecaident', 'Api@devolverpecaident');
+
+
